96e5334bbd32461764b91212fc57a43f69ec914f
Montana Rowe <calcnerd256@gmail.com>
2014-10-27 12:19:05 +0000

    use apache to serve static files

--- telelogue/settings/__init__.py
+++ telelogue/settings/__init__.py
@@ -20,6 +20,8 @@ BASE_PATH = path.dirname( # /path/to/project/repo/
         )
     )
 )
+SERVER_PATH = "/var/www"
+PROD_PATH = path.join(SERVER_PATH, "telelogue")
 
 DATABASES = {
     'default': {
@@ -75,11 +77,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.join(SERVER_PATH, "html/static/telelogue/")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static/telelogue/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
