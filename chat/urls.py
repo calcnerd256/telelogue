558b1646310c7757aa3d5c211a87f9680736261b
Montana Rowe <calcnerd256@gmail.com>
2014-11-12 02:46:03 +0000

    login_required for sensitive pages

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from chat.views import (
     ChatHomeView,
     MessageCreateView,
@@ -11,6 +12,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', ChatHomeView.as_view(), name='home'),
-    url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
-    url(r'^message/list/$', MessageListView.as_view(), name='message_list'),
+    url(r'^message/create/$', login_required(MessageCreateView.as_view()), name='message_create'),
+    url(r'^message/list/$', login_required(MessageListView.as_view()), name='message_list'),
 )
