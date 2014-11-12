422123f4eaee7a6e069b3136abd49905c5788413
Alaina Troiano <alaina.troiano@gmail.com>
2014-11-11 19:22:57 -0600

    Stubbed message create and list views

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from chat.views import ChatHomeView
+from chat.views import (
+    ChatHomeView,
+    MessageCreateView,
+    MessageListView,
+)
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', ChatHomeView.as_view(), name='home'),
+    url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
+    url(r'^message/list/$', MessageListView.as_view(), name='message_list'),
 )
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView,
+    CreateView,
+    ListView,
+)
+from django.core.urlresolvers import reverse
+from models import ChatMessage
 
 
 class ChatHomeView(TemplateView):
     template_name = 'chat/home.html'
+
+
+class MessageCreateView(CreateView):
+    model = ChatMessage
+
+    def get_success_url(self):
+        return reverse('message_list')
+
+
+class MessageListView(ListView):
+    model = ChatMessage
+    paginate_by = 20
