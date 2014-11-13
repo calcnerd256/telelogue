149ed8b8a2ddd23e1d6b9ca3392f8047a7de8126
Montana Rowe <calcnerd256@gmail.com>
2014-11-13 05:24:32 +0000

    message create view

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,21 @@
 from django.views.generic import (
     TemplateView,
+    CreateView,
     ListView,
 )
+from django.core.urlresolvers import reverse
 from models import ChatMessage
 
 
 class ChatHomeView(TemplateView):
     template_name = 'chat/home.html'
+
+
+class MessageCreateView(CreateView):
+    model = ChatMessage
+
+    def get_success_url(self):
+        return reverse('message_list')
 
 
 class MessageListView(ListView):
