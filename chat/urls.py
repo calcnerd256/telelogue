c7ec992c71c791e99388ef49bf51e4d4309e2bcd
Montana Rowe <calcnerd256@gmail.com>
2014-11-13 05:30:00 +0000

    message_create url and links for message create view

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from chat.views import (
     ChatHomeView,
+    MessageCreateView,
     MessageListView,
 )
 
@@ -10,5 +11,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', ChatHomeView.as_view(), name='home'),
+    url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
     url(r'^message/list/$', MessageListView.as_view(), name='message_list'),
 )
