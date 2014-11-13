e77fc14bb583ca52d41e95d4dfacff9172ffc77a
Montana Rowe <calcnerd256@gmail.com>
2014-11-13 05:02:04 +0000

    chat message list view

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,18 @@
 from django.views.generic import (
     TemplateView,
+    ListView,
 )
+from models import ChatMessage
 
 
 class ChatHomeView(TemplateView):
     template_name = 'chat/home.html'
+
+
+class MessageListView(ListView):
+    model = ChatMessage
+    paginate_by = 20
+
+    def get_queryset(self):
+        qs = super(MessageListView, self).get_queryset()
+        return qs.order_by("-timestamp")
