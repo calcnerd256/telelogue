14b30192418a05a84c52b8ba98470a7fc1c872b0
Montana Rowe <calcnerd256@gmail.com>
2014-11-13 02:54:57 +0000

    require login

--- chat/urls.py
+++ chat/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
 
     url(r'^message/create/$', login_required(MessageCreateView.as_view()), name='message_create'),
     url(r'^message/list/$', login_required(MessageListView.as_view()), name='message_list'),
-    url(r'^message/(?P<pk>\d+)/$', MessageDetailView.as_view(), name='message_detail'),
+    url(r'^message/(?P<pk>\d+)/$', login_required(MessageDetailView.as_view()), name='message_detail'),
 
-    url(r'^user/(?P<pk>\d+)/$', UserDetailView.as_view(), name='user_detail'),
+    url(r'^user/(?P<pk>\d+)/$', login_required(UserDetailView.as_view()), name='user_detail'),
 )
