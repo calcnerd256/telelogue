b9cf71cc59a36d543b0b451e4e05009a5437d49b
Montana Rowe <calcnerd256@gmail.com>
2015-01-19 15:13:33 +0000

    nullable fks on transit edges

--- transit/models.py
+++ transit/models.py
@@ -3,8 +3,8 @@ from cuser.fields import CurrentUserField
 from chat.models import ChatMessage
 
 class Triple(models.Model):
-    source = models.ForeignKey(ChatMessage, related_name="source_set")
-    path = models.ForeignKey(ChatMessage, related_name="path_set")
-    destination = models.ForeignKey(ChatMessage, related_name="destination_set")
+    source = models.ForeignKey(ChatMessage, related_name="source_set", null=True)
+    path = models.ForeignKey(ChatMessage, related_name="path_set", null=True)
+    destination = models.ForeignKey(ChatMessage, related_name="destination_set", null=True)
     author = CurrentUserField(add_only=True)
     timestamp = models.DateTimeField(auto_now_add=True)
