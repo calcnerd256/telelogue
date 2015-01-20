ac449d3d5bd6f6b8b85e16bcbcc166fba2c05adc
Montana Rowe <calcnerd256@gmail.com>
2015-01-20 12:21:10 +0000

    reuse main chat list for fringe

--- transit/views.py
+++ transit/views.py
@@ -8,11 +8,12 @@ from .models import (
     Triple,
 )
 from chat.models import ChatMessage
+from chat.views import MessageListView
 
-class UnmetSemanticsView(TemplateView):
+class UnmetSemanticsView(MessageListView):
     template_name = "transit/unmet_semantics.html"
 
-    def get_queryset(self):
+    def get_candidates(self):
         def filter_step(name):
             if Triple.lookup_semantic(name) is not None: return False
             source_name, path_name = lookup_semantics[name][0:2]
@@ -40,7 +41,8 @@ class UnmetSemanticsView(TemplateView):
             }
         return [map_step(name) for name in lookup_semantics.keys() if filter_step(name)]
 
+
     def get_context_data(self, *args, **kwargs):
         context = super(UnmetSemanticsView, self).get_context_data(*args, **kwargs)
-        context["object_list"] = self.get_queryset()
+        context["semantic_fringe"] = self.get_candidates()
         return context
