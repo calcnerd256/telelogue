0e925c2f0574db7b8b55a65e38959300856e0beb
Montana Rowe <calcnerd256@gmail.com>
2015-01-20 13:50:38 +0000

    a view that takes message IDs and creates an edge

--- transit/urls.py
+++ transit/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from .views import (
     UnmetSemanticsView,
+    CreateFromThreeMessagesView,
 )
 
 admin.autodiscover()
@@ -15,5 +16,12 @@ urlpatterns = patterns(
             UnmetSemanticsView.as_view()
         ),
         name="unmet_semantics",
+    ),
+    url(
+        r'^triple/create_from/(?P<source>\d+)/(?P<path>\d+)/(?P<destination>\d+)/$',
+        login_required(
+            CreateFromThreeMessagesView.as_view()
+        ),
+        name="create_from_three_messages",
     ),
 )
--- transit/views.py
+++ transit/views.py
@@ -1,14 +1,80 @@
 # Create your views here.
 
 from django.views.generic import (
-    TemplateView,
+    CreateView,
 )
+from django.forms import HiddenInput
+from django.shortcuts import (
+    get_object_or_404,
+)
+from django.core.urlresolvers import (
+   reverse_lazy,
+)
+
 from .models import (
     lookup_semantics,
     Triple,
 )
 from chat.models import ChatMessage
 from chat.views import MessageListView
+
+def cache_getter(name):
+    def decorator(fn):
+        def decorated(self, *args, **kwargs):
+            if hasattr(self, name): return getattr(self, name)
+            result = fn(self, *args, **kwargs)
+            setattr(self, name, result)
+            return result
+        return decorated
+    return decorator
+
+class CreateFromThreeMessagesView(CreateView):
+    model = Triple
+    template_name = "transit/triple/create/from_messages.html"
+    success_url = reverse_lazy("unmet_semantics")
+
+    @cache_getter("source")
+    def get_source(self):
+        pk = self.kwargs["source"]
+        if pk == "0": return None
+        return get_object_or_404(ChatMessage, pk=pk)
+
+    @cache_getter("path")
+    def get_path(self):
+        pk = self.kwargs["path"]
+        if pk == "0": return None
+        return get_object_or_404(ChatMessage, pk=pk)
+
+    @cache_getter("destination")
+    def get_destination(self):
+        pk = self.kwargs["destination"]
+        if pk == "0": return None
+        return get_object_or_404(ChatMessage, pk=pk)
+
+    def get_initial(self, *args, **kwargs):
+        result = super(CreateFromThreeMessagesView, self).get_initial(*args, **kwargs)
+        result["source"] = self.get_source()
+        result["path"] = self.get_path()
+        result["destination"] = self.get_destination()
+        return result
+
+    def get_form_class(self, *args, **kwargs):
+        result = super(CreateFromThreeMessagesView, self).get_form_class(*args, **kwargs)
+        source = result.base_fields["source"]
+        path = result.base_fields["path"]
+        destination = result.base_fields["destination"]
+        source.widget = HiddenInput()
+        path.widget = HiddenInput()
+        destination.widget = HiddenInput()
+        return result
+
+    def get_context_data(self, *args, **kwargs):
+        result = super(CreateFromThreeMessagesView, self).get_context_data(*args, **kwargs)
+        result["source"] = self.get_source()
+        result["path"] = self.get_path()
+        result["destination"] = self.get_destination()
+        return result
+
 
 class UnmetSemanticsView(MessageListView):
     template_name = "transit/unmet_semantics.html"
