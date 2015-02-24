256e1d496c210a281ee1b3c9efe65937fa4aa3c5
Montana Rowe <calcnerd256@gmail.com>
2015-02-24 14:31:49 +0000

    brought in chatmessage symbolic expression export and cleaned up the template fragment a little

---
+++ chat/templates/export/chat/chatmessage_detail.lisp
@@ -0,0 +1,11 @@
+  (message
+   :time {{ object.timestamp|"U" }} :timezone "{{ object.timestamp|"O" }}"{% comment %}"{% endcomment %}
+   :author {% include "export/auth/user.lisp" with object=object.author only %} :authenticity nil
+   :unicode {% with points=object.get_body_codepoints %}{% comment %}
+    {% endcomment %}{% if points %}(list{% comment %}
+     {% endcomment %}{% if points|length < 16 %}{% for point in points %} point{% endfor %}){% comment %}
+     {% endcomment %}{% else %}{% for point in points %} {{ point }}{% if forloop.counter|divisibleby:"16" %}
+   {% endif %}{% endfor %}
+   ){% endif %}{% else %}nil{% endif %}{% endwith %}
+   :id {{ object.id }}
+  )
