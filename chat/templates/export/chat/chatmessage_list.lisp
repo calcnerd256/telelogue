fd7a0c3b8923f0aaf6c94fb7755bbdb147cb06ed
Montana Rowe <calcnerd256@gmail.com>
2015-02-24 14:58:48 +0000

    whitespace

--- chat/templates/export/chat/chatmessage_list.lisp
+++ chat/templates/export/chat/chatmessage_list.lisp
@@ -1,11 +1,11 @@
 (telelogue{% if is_paginated %}
  :paginate (
   pagination
-   :page {{ page_obj.number }}
-   :of {{ page_obj.paginator.num_pages }}
+  :page {{ page_obj.number }}
+  :of {{ page_obj.paginator.num_pages }}
  ){% endif %}
  :messages (
   list
-{% for object in object_list%}{% include "export/chat/chatmessage_detail.lisp" %}{% endfor %} )
+{% for object in object_list %}{% include "export/chat/chatmessage_detail.lisp" %}{% endfor %} )
  :server nil
 )
