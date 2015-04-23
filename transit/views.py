6a7985e3f56941cab7c4106534221f37c2ba7a68
Montana Rowe <calcnerd256@gmail.com>
2015-04-23 12:58:52 +0000

    lookup_semantics is a dict, not a callable

--- transit/views.py
+++ transit/views.py
@@ -121,7 +121,7 @@ class UnmetSemanticsView(MessageListView):
                 "source": source,
                 "path": path,
             }
-        names = lookup_semantics().keys()
+        names = lookup_semantics.keys()
         return map(map_step, filter(filter_step, names))
 
 
