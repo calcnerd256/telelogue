de12053d899b8fdc3e41211a605b711027c37793
Montana Rowe <calcnerd256@gmail.com>
2015-08-19 20:18:48 +0000

    moved a mixin into base

--- transit/views/base.py
+++ transit/views/base.py
@@ -1,3 +1,7 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import models
+from django.shortcuts import get_object_or_404
+
 from chat.views import PageTitleMixin
 from chat.models import ChatMessage
 
@@ -44,3 +48,36 @@ class EnhancedViewMixin(PageTitleMixin, NextOnSuccessMixin):
 
 class EnhancedMessageMixin(EnhancedViewMixin):
     model = ChatMessage
+
+
+class ObjectAndListView(ListView):
+    pk_url_kwarg = "pk"
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            self.model,
+            pk=int(self.kwargs.get(self.pk_url_kwarg))
+        )
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object_list = self.get_queryset()
+        context = self.get_context_data(
+            object=self.object,
+            object_list=self.object_list,
+            **kwargs
+        )
+        return self.render_to_response(context)
+
+    def get_context_data(self, *args, **kwargs):
+        context = {}
+        obj = self.object
+        context_object_name = None
+        if isinstance(obj, models.Model):
+            if self.object._deferred:
+                obj = obj._meta.proxy_for_model
+            context_object_name = obj._meta.object_name.lower()
+        if context_object_name:
+            context[context_object_name] = self.object
+        context.update(**kwargs)
+        return super(ObjectAndListView, self).get_context_data(*args, **context)
