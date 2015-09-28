dd73d40faeb29add6ddd91bfd97a6ad3f320a7d8
Montana Rowe <calcnerd256@gmail.com>
2015-09-28 00:12:43 +0000

    show avunculars in thread reply view

--- transit/views/reply.py
+++ transit/views/reply.py
@@ -16,14 +16,26 @@ class ReplyView(EnhancedMessageMixin, CreateView):
         pk = self.kwargs.get("parent")
         return self.model.objects.get(pk=pk)
 
-    def get_siblings(self):
-        parent = self.get_parent()
-        reply = Triple.lookup_semantic("reply")
-        if reply is None: return []
-        destinations = [(trip, trip.current_value()) for trip in parent.destination_set.all()]
-        destinations = [t for (t, d) in destinations if d is not None and d.pk == parent.pk]
-        paths = [trip.path for trip in destinations if trip.source is not None and trip.source.pk == reply.pk]
-        return self.model.objects.filter(pk__in=set(path.pk for path in paths if path is not None)).order_by("-timestamp")
+    def get_siblings(self, parent=None):
+        if parent is None:
+            parent = self.get_parent()
+        paths = [t.path for t in parent.destination_set.all()]
+        sibling_pks = [p.pk for p in filter(None, paths) if p.parent and p.parent.pk == parent.pk]
+        result = self.model.objects.filter(pk__in=sibling_pks).order_by("timestamp")
+        return result
+
+    def trisect_siblings(self, message):
+        parent = message.parent
+        message.children = self.get_siblings(message)
+        if parent is None:
+            return [], message, []
+        result = list(self.get_siblings(parent))
+        for m in result:
+            m.children = list(self.get_siblings(m))
+        index = [i for i,m in enumerate(result) if m.pk == message.pk][0]
+        left = result[:index]
+        right = result[index+1:]
+        return left, message, right
 
     def form_valid(self, form):
         reply = Triple.lookup_semantic("reply")
@@ -42,6 +54,8 @@ class ReplyView(EnhancedMessageMixin, CreateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(ReplyView, self).get_context_data(*args, **kwargs)
-        context["parent"] = self.get_parent()
+        parent = self.get_parent()
+        context["parent"] = parent
         context["object_list"] = self.get_siblings()
+        context["ancestors"] = map(self.trisect_siblings, parent.get_ancestors())
         return context
